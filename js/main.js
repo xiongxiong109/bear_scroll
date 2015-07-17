1a30d78feb66f49b42231f5291ebb354759f66c0
xiongxiong109 <444122662@qq.com>
2015-07-17 14:50:03 +0800

    fixed a scroll bug

--- js/main.js
+++ js/main.js
@@ -1,30 +1,46 @@
 $(function(){
 
 	var iCur=0;
+	var isRunnig=false;
+
+	$(".page").on("pageIn",function(){
+
+		isRunnig=false;
+		//利用自定义事件来保证动画的单例执行
+	});
 
 	$(".page").on("swipeUp",function(){
 
-		if(iCur>= $(".page").length-1 ){
-			iCur=0;
+		if(!isRunnig){
+			isRunnig=true;
+			if(iCur>= $(".page").length-1 ){
+				iCur=0;
+			}
+			else{
+				iCur++;
+			}
+			swipePage( $(this), "coverOutUp",'out' );
+			swipePage( $(".page").eq(iCur),"slideInUp",'in' );
 		}
-		else{
-			iCur++;
-		}
-		swipePage( $(this), "coverOutUp",'out' );
-		swipePage( $(".page").eq(iCur),"slideInUp",'in' );
+
 
 	});
 
 	$(".page").on("swipeDown",function(){
 
-		if(iCur<=0){
-			iCur=$(".page").length-1;
+		if( !isRunnig ){
+
+			isRunnig=true;
+			if(iCur<=0){
+				iCur=$(".page").length-1;
+			}
+			else{
+				iCur--;
+			}
+			swipePage( $(this), "coverOutDown",'out' );
+			swipePage( $(".page").eq(iCur),"slideInDown",'in' );
+
 		}
-		else{
-			iCur--;
-		}
-		swipePage( $(this), "coverOutDown",'out' );
-		swipePage( $(".page").eq(iCur),"slideInDown",'in' );
 
 	});
 
